1e54ce649f25f97e1afbdd33c7b8850b711cc1a3
Jhonny Knaak de Vargas <jhonny.vargas@acad.pucrs.br>
2020-05-12 16:43:57 +0100

    wordsearch

--- src/wordsearch.rs
+++ src/wordsearch.rs
@@ -1,7 +1,8 @@
 struct Solution;
 
 impl Solution {
-    pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
+    // O(board.len() * board[0].len() * 4 ^ word.len())
+    pub fn exist(mut board: Vec<Vec<char>>, word: String) -> bool {
         let i_len = board.len();
         let j_len = board[0].len();
 
@@ -10,10 +11,8 @@ impl Solution {
 
         for i in 0..i_len {
             for j in 0..j_len {
-                if board[i][j] == letters[letter_pos] {
-                    if Self::check_around(&letters, letter_pos + 1, &board, i, j) {
-                        return true;
-                    }
+                if Self::check_around(&letters, 0, &mut board, i as i32, j as i32) {
+                    return true;
                 }
             }
         }
@@ -23,51 +22,34 @@ impl Solution {
 
     fn check_around(letters: &Vec<char>,
                     letter_pos: usize,
-                    board: &Vec<Vec<char>>,
-                    i: usize,
-                    j: usize) -> bool {
-        if letter_pos >= letters.len() {
+                    board: &mut Vec<Vec<char>>,
+                    i: i32,
+                    j: i32) -> bool {
+        if letter_pos == letters.len() {
             return true;
         }
-        let looking_for = letters[letter_pos];
 
-        let mut new_i = Self::add(1, i, board.len());
-        if board[new_i][j] == looking_for {
-            return Self::check_around(letters, letter_pos + 1, board, new_i, j);
+        if i < 0 ||
+            j < 0 ||
+            i as usize >= board.len() ||
+            j as usize >= board[0].len() ||
+            board[i as usize][j as usize] != letters[letter_pos] {
+            return false;
         }
 
-        new_i = Self::add(-1, i, board.len());
-        if board[new_i][j] == looking_for {
-            return Self::check_around(letters, letter_pos + 1, board, new_i, j);
+        board[i as usize][j as usize] == '#';
+
+
+        if Self::check_around(letters, letter_pos + 1, board, i + 1, j)
+            || Self::check_around(letters, letter_pos + 1, board, i - 1, j)
+            || Self::check_around(letters, letter_pos + 1, board, i, j + 1)
+            || Self::check_around(letters, letter_pos + 1, board, i, j - 1) {
+            return true;
         }
 
-        let mut new_j = Self::add(1, j, board[0].len());
-        if board[i][new_j] == looking_for {
-            return Self::check_around(letters, letter_pos + 1, board, i, new_j);
-        }
+        board[i as usize][j as usize] = letters[letter_pos];
 
-        new_j = Self::add(-1, j, board[0].len());
-        if board[i][new_j] == looking_for {
-            return Self::check_around(letters, letter_pos + 1, board, i, new_j);
-        }
-
-        return false;
-    }
-
-    fn add(val: i32, target: usize, board_len: usize) -> usize {
-        if val > 0 {
-            if target + (val as usize) > board_len {
-                return 0;
-            } else {
-                return target + (val as usize);
-            }
-        } else {
-            if (target as i32) + val < 0 {
-                return board_len - 1;
-            } else {
-                return target - (val as usize);
-            }
-        }
+        false
     }
 }
 
@@ -79,7 +61,7 @@ mod tests {
     fn exist_1() {
         assert_eq!(Solution::exist(vec![
             vec!['A','B','C','E'],
-            vec!['A','B','C','E'],
+            vec!['S','F','C','S'],
             vec!['A','D','E','E']], "ABCCED".to_string()), true);
     }
 
@@ -87,7 +69,7 @@ mod tests {
     fn exist_2() {
         assert_eq!(Solution::exist(vec![
             vec!['A','B','C','E'],
-            vec!['A','B','C','E'],
+            vec!['S','F','C','S'],
             vec!['A','D','E','E']], "SEE".to_string()), true);
     }
 
@@ -95,7 +77,7 @@ mod tests {
     fn exist_3() {
         assert_eq!(Solution::exist(vec![
             vec!['A','B','C','E'],
-            vec!['A','B','C','E'],
+            vec!['S','F','C','S'],
             vec!['A','D','E','E']], "ABCB".to_string()), false);
     }
 }
