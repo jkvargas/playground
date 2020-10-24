f422280ec628ee3648721806f77482f1a1dae1e5
Jhonny Vargas <vargasj@ryanair.com>
2020-10-24 17:11:26 +0100

    It worked =D

--- src/datastructs/mod.rs
+++ src/datastructs/mod.rs
@@ -1,1 +1,26 @@
-mod trie;
+use std::error::Error;
+use std::fmt;
+use std::fmt::{Display, Formatter};
+
+mod trienode;
+
+#[derive(Debug)]
+pub enum TrieError {
+    EmptyString,
+}
+
+pub type TrieResult<TResult> = Result<TResult, TrieError>;
+
+impl Display for TrieError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            TrieError::EmptyString => write!(f, "Empty string"),
+        }
+    }
+}
+
+impl Error for TrieError {}
+
+pub trait TrieT {
+    fn insert(&mut self, key: String) -> TrieResult<()>;
+}
