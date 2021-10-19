302e328490a74e270305fbc10b1633e12de425fe
Jhonny Knaak de Vargas <vargasj@ryanair.com>
2021-10-19 15:31:24 +0100

    second time.

--- src/nodes/binarytreezigzag.rs
+++ src/nodes/binarytreezigzag.rs
@@ -1,57 +1,98 @@
 use crate::nodes::TreeNode;
-use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::rc::Rc;
 
+// struct Solution;
+//
+// impl Solution {
+//     pub fn zigzag_level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+//         let mut result: Vec<Vec<i32>> = Vec::new();
+//
+//         if root.is_none() {
+//             return result;
+//         }
+//
+//         let mut level = VecDeque::new();
+//         let mut queue = VecDeque::new();
+//
+//         queue.push_front(Some(root.unwrap()));
+//         queue.push_front(None);
+//
+//         let mut from_left = true;
+//
+//         while !queue.is_empty() {
+//             if let Some(node) = queue.pop_back().unwrap() {
+//                 let borrowed: &RefCell<TreeNode> = node.borrow();
+//                 let val = borrowed.borrow();
+//
+//                 if from_left {
+//                     level.push_front(val.val);
+//                 } else {
+//                     level.push_back(val.val);
+//                 }
+//
+//                 if val.left.is_some() {
+//                     queue.push_front(val.left.clone());
+//                 }
+//                 if val.right.is_some() {
+//                     queue.push_front(val.right.clone());
+//                 }
+//             } else {
+//                 let mut vec_result = Vec::new();
+//                 while !level.is_empty() {
+//                     vec_result.push(level.pop_back().unwrap());
+//                 }
+//                 result.push(vec_result);
+//
+//                 level = VecDeque::new();
+//
+//                 if !queue.is_empty() {
+//                     queue.push_front(None);
+//                 }
+//                 from_left = !from_left;
+//             }
+//         }
+//
+//         result
+//     }
+// }
+
 struct Solution;
+
+const RIGHT_TO_LEFT: i32 = 2;
+const LEFT_TO_RIGHT: i32 = 1;
 
 impl Solution {
     pub fn zigzag_level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
-        let mut result: Vec<Vec<i32>> = Vec::new();
+        let mut queue = Vec::new();
+        let mut result = Vec::new();
 
-        if root.is_none() {
-            return result;
-        }
-
-        let mut level = VecDeque::new();
-        let mut queue = VecDeque::new();
-
-        queue.push_front(Some(root.unwrap()));
-        queue.push_front(None);
-
-        let mut from_left = true;
+        queue.push((root.clone(), 0, LEFT_TO_RIGHT));
 
         while !queue.is_empty() {
-            if let Some(node) = queue.pop_back().unwrap() {
-                let borrowed: &RefCell<TreeNode> = node.borrow();
-                let val = borrowed.borrow();
+            let (node, level, dir) = queue.pop().unwrap();
 
-                if from_left {
-                    level.push_front(val.val);
-                } else {
-                    level.push_back(val.val);
+            if let Some(n) = node {
+                let bor = n.borrow();
+
+                if result.len() < level + 1 {
+                    result.push(vec![]);
                 }
 
-                if val.left.is_some() {
-                    queue.push_front(val.left.clone());
+                match dir {
+                    LEFT_TO_RIGHT => {
+                        queue.push((bor.left.clone(), level + 1, RIGHT_TO_LEFT));
+                        queue.push((bor.right.clone(), level + 1, RIGHT_TO_LEFT));
+                    }
+                    RIGHT_TO_LEFT => {
+                        queue.push((bor.right.clone(), level + 1, LEFT_TO_RIGHT));
+                        queue.push((bor.left.clone(), level + 1, LEFT_TO_RIGHT));
+                    }
+                    _ => {}
                 }
-                if val.right.is_some() {
-                    queue.push_front(val.right.clone());
-                }
-            } else {
-                let mut vec_result = Vec::new();
-                while !level.is_empty() {
-                    vec_result.push(level.pop_back().unwrap());
-                }
-                result.push(vec_result);
 
-                level = VecDeque::new();
-
-                if !queue.is_empty() {
-                    queue.push_front(None);
-                }
-                from_left = !from_left;
+                result[level].push(bor.val);
             }
         }
 
