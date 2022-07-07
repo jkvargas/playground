49d4e54c50be82025ae4aadbb97b79c7d13a0d83
Jhonny Knaak de Vargas <jhonny.knaak@protonmail.com>
2022-07-07 17:00:00 -0400

    sudoku

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -4,22 +4,94 @@ use std::collections::HashSet;
 
 struct Solution;
 
-impl Solution {
-    pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
-        let mut cols: Vec<HashSet<char>> = vec![HashSet::new(); board[0].len()];
-        let mut lines: Vec<HashSet<char>> = vec![HashSet::new(); board.len()];
-        let mut boxes: Vec<HashSet<char>> = vec![HashSet::new(); board[0].len() / 3 * board.len() / 3];
+const BOX_SIZE: usize = 3;
 
-        for i in 0..board.len() {
-            for j in 0..board[0].len() {
-                let box_number = i % 3 + j % 3;
+struct Sudoku<'a> {
+    board: &'a mut Vec<Vec<char>>,
+    cols: Vec<HashSet<char>>,
+    lines: Vec<HashSet<char>>,
+    boxes: Vec<Vec<HashSet<char>>>,
+    free_positions: Vec<Vec<usize>>,
+}
 
-                if vec[i][j] != '.' {
-                    cols[j].insert(vec[i][j]);
-                    lines[i].insert(vec[i][j]);
+impl<'a> Sudoku<'a> {
+    pub fn new(board: &'a mut Vec<Vec<char>>) -> Self {
+        let lines_len = board.len();
+        let cols_len = board[0].len();
+        let mut sudoku = Sudoku {
+            free_positions: Vec::new(),
+            board,
+            lines: vec![HashSet::new(); lines_len],
+            cols: vec![HashSet::new(); cols_len],
+            boxes: vec![vec![HashSet::new(); cols_len / BOX_SIZE]; lines_len / BOX_SIZE],
+        };
 
+        for i in 0..sudoku.board.len() {
+            for j in 0..sudoku.board[0].len() {
+                if sudoku.board[i][j] != '.' {
+                    sudoku.lines[i].insert(sudoku.board[i][j]);
+                    sudoku.cols[j].insert(sudoku.board[i][j]);
+                    sudoku.boxes[i / BOX_SIZE][j / BOX_SIZE].insert(sudoku.board[i][j]);
+                } else {
+                    sudoku.free_positions.push(vec![i, j]);
                 }
             }
         }
+
+        sudoku
+    }
+
+    pub fn solve(&mut self) {
+        self.try_fill(0);
+    }
+
+    fn try_fill(&mut self, pos: usize) -> bool {
+        if pos == self.free_positions.len() {
+            return true;
+        }
+
+        for i in 1..10 {
+            if self.can_place(i, pos) {
+                self.put(i, pos);
+                if self.try_fill(pos + 1) {
+                    return true;
+                }
+                self.remove_number(pos);
+            }
+        }
+
+        false
+    }
+
+    fn remove_number(&mut self, pos: usize) {
+        let i = self.free_positions[pos][0];
+        let j = self.free_positions[pos][1];
+        self.lines[i].remove(&self.board[i][j]);
+        self.cols[j].remove(&self.board[i][j]);
+        self.boxes[i / BOX_SIZE][j / BOX_SIZE].remove(&self.board[i][j]);
+        self.board[i][j] = '.';
+    }
+
+    fn can_place(&self, number: u32, pos: usize) -> bool {
+        let letter = char::from_digit(number, 10).unwrap();
+        let i = self.free_positions[pos][0];
+        let j = self.free_positions[pos][1];
+        !self.lines[i].contains(&letter) && !self.cols[j].contains(&letter) && !self.boxes[i / BOX_SIZE][j / BOX_SIZE].contains(&letter)
+    }
+
+    fn put(&mut self, number: u32, pos: usize) {
+        let i = self.free_positions[pos][0];
+        let j = self.free_positions[pos][1];
+        self.board[i][j] = char::from_digit(number, 10).unwrap();
+        self.lines[i].insert(self.board[i][j]);
+        self.cols[j].insert(self.board[i][j]);
+        self.boxes[i / BOX_SIZE][j / BOX_SIZE].insert(self.board[i][j]);
+    }
+}
+
+impl Solution {
+    pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+        let mut sudoku = Sudoku::new(board);
+        sudoku.solve();
     }
 }
