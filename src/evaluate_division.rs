61a6fc598b57255e7e7767d4d91cb79cb22e839c
Jhonny Knaak de Vargas <jhonny.knaak@gmail.com>
2025-02-03 22:01:06 -0800

    studying

---
+++ src/evaluate_division.rs
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/evaluate-division/description/?envType=study-plan-v2&envId=top-interview-150
+
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn calc_equation(
+        equations: Vec<Vec<String>>,
+        values: Vec<f64>,
+        queries: Vec<Vec<String>>,
+    ) -> Vec<f64> {
+        let mut map: HashMap<String, HashMap<String, f64>> = HashMap::new();
+
+        for i in 0..equations.len() {
+            map.entry(equations[i][0].clone()).or_insert(HashMap::new()).entry(equations[i][1].clone()).or_insert(values[i]);
+            map.entry(equations[i][1].clone()).or_insert(HashMap::new()).entry(equations[i][0].clone()).or_insert(1. / values[i]);
+        }
+
+        let mut result = Vec::new();
+        
+        for i in 0..queries.len() {
+            if let Some(first) = map.get(&queries[i][0]) {
+                
+            } else {
+               result.push(-1.0);  
+            }
+        }
+        
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::evaluate_division::Solution;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            vec![6.00000, 0.50000, -1.00000, 1.00000, -1.00000],
+            Solution::calc_equation(
+                vec![
+                    vec!["a".to_string(), "b".to_string()],
+                    vec!["b".to_string(), "c".to_string()]
+                ],
+                vec![2.0, 3.0],
+                vec![
+                    vec!["a".to_string(), "c".to_string()],
+                    vec!["b".to_string(), "a".to_string()],
+                    vec!["a".to_string(), "e".to_string()],
+                    vec!["a".to_string(), "a".to_string()],
+                    vec!["x".to_string(), "x".to_string()]
+                ]
+            )
+        );
+    }
+}
