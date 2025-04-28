e6922960a5c41538c14cc77fe32613dfa0a86ad6
Jhonny Knaak de Vargas <jhonny.knaakdevargas@sony.com>
2025-04-27 21:55:55 -0700

    subs

--- src/minimum_window_substring.rs
+++ src/minimum_window_substring.rs
@@ -1,30 +1,67 @@
 // https://leetcode.com/explore/interview/card/facebook/5/array-and-strings/285/
 
+use std::cmp::min;
 use std::collections::{HashMap, HashSet};
 
 struct Solution;
 
 impl Solution {
     pub fn min_window(s: String, t: String) -> String {
-        let letters = s.chars().collect::<Vec<char>>();
-        let map = t.chars().collect::<HashSet<char>>();
-        let mut resulting_map = HashMap::new();
-        
-        for i in 0..letters.len() {
-            if map.contains(&letters[i]) {
-                resulting_map.entry(letters[i]).or_insert(Vec::new()).push(i);
+        let mut a = 0;
+        let mut b = 0;
+        let mut cur_min = i32::max_value();
+        let mut overall_min = i32::max_value();
+        let mut start = 0;
+        let mut end = 0;
+
+        let letters = s.chars().collect::<Vec<_>>();
+        let mut map = t.chars().map(|c| (c, 0)).collect::<HashMap<char, i32>>();
+        let mut shorten = false;
+        while b < s.len() && !shorten {
+            if shorten {
+                map.entry(letters[a]).and_modify(|x| *x -= 1);
+                a += 1;
+                if !is_valid(&map) {
+                    shorten = false;
+                } else {
+                    cur_min = b as i32 - a as i32;
+                    if cur_min < overall_min {
+                        overall_min = cur_min;
+                        start = a;
+                        end = b;
+                    }
+                }
+            }
+            else {
+                map.entry(letters[b]).and_modify(|v| *v += 1).or_insert(1);
+                b += 1;
+                if is_valid(&map) {
+                    shorten = true;
+                    cur_min = b as i32 - a as i32;
+
+                    if cur_min < overall_min {
+                        overall_min = cur_min;
+                        start = a;
+                        end = b;
+                    }
+                }
             }
         }
-        
-        
-        
-        "oo".to_string()
+
+        s[start..end].to_string()
     }
 }
 
+fn is_valid(map: &HashMap<char, i32>) -> bool {
+    map.iter().all(|(_, &c)| c > 0)
+}
 
-// "ADOBECODEBANC", t = "ABC"
+#[cfg(test)]
+mod tests {
+    use crate::minimum_window_substring::Solution;
 
-A - 0, 10,
-B - 3, 9,
-C - 5, 12
+    #[test]
+    fn example_1() {
+        assert_eq!("BANC".to_string(), Solution::min_window("ADOBECODEBANC".to_owned(), "ABC".to_owned()))
+    }
+}
